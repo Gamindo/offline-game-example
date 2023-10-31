2903972d1165eb89237d58707fb1ab757cd924d9
Pasquale Ragozzino <pasquale.ragozzino89@gmail.com>
2023-10-31 12:09:17 +0100

    Fist commit

---
+++ app.js
@@ -0,0 +1,24 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function (registration) {
+      console.log("Service Worker registrato con successo:", registration);
+    })
+    .catch(function (error) {
+      console.log("La registrazione del Service Worker non è riuscita:", error);
+    });
+}
+
+document.addEventListener("keydown", jump);
+
+function jump(event) {
+  if (event.keyCode === 32) {
+    // spazio
+    let player = document.getElementById("player");
+    player.style.animation = "jump 0.5s";
+  }
+}
+
+document.getElementById("player").addEventListener("animationend", () => {
+  document.getElementById("player").style.animation = "";
+});
