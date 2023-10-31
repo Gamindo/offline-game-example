da5c40de29c5ad43cdabe67db39259136f311aa3
Pasquale Ragozzino <pasquale.ragozzino89@gmail.com>
2023-10-31 12:34:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/sw.js", { scope: "/offline-game-example" })
+    .register("/sw.js", { scope: "/offline-game-example/sw.js" })
     .then(function (registration) {
       console.log("Service Worker registrato con successo:", registration);
     })
